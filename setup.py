459d359a3165ba99636dffc152fa66d3c3c2c290
hariesramdhani <hydrolizedmaltose@gmail.com>
2024-09-05 13:23:36 +0100

    Refactored

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_package_name',
+    name='scrapplier',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
@@ -11,14 +11,14 @@ setup(
     ],
     author='Haries Ramdhani',
     author_email='hydrolizedmaltose@gmail.com',
-    description='A short description of your package',
+    description='A python package to scrape school and products information from the supplier website',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/your-repo',
+    url='https://github.com/hariesramdhani/scrapplier',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.9.6',
 )
